32e964cb69bf5fa2243ef899d5d6addda926064a
Nicholas Bleeker <cosmicdawg32@gmail.com>
2025-10-27 13:34:04 -0400

    Finished code

--- utils.h
+++ utils.h
@@ -5,11 +5,12 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <arpa/inet.h>
-
+#define EPS 1e-3
 
 struct msg{
     float T;     // Temperature 
     int Index;    // Index indifying the process 
+    int Done;	// 0 = keep going, 1 = stop
 }; 
 
 struct msg prepare_message(int i_Index, float i_Temperature); 
